53f728af53b9d287bcd13aa65abd2b3e48a0434f
Viswajit Nayak <pragmaticde@Viswajits-Mac-mini.local>
2022-06-12 19:02:01 +0530

    Learn C the hard way

--- src/Exercise7.c
+++ src/Exercise7.c
@@ -11,16 +11,24 @@ int main(int argc, char *argv[])
 
     printf("You are %d miles away.\n", distance);
     printf("You are %f levels of power.\n", power);
-    printf("You have %f awesome super powers.\n", super_power);
+    printf("You have %f awesome super powers.\n\n", super_power);
     printf("I have an initial %c.\n", initial);
     printf("I have the first name \t==> %s \n", first_name);
     printf("I have the last name \t==> %s\n", last_name);
-    printf("My whole name is \t==> %s %c. %s\n", first_name, initial, last_name);
+    printf("My whole name is \t==> %s %c. %s\n\n", first_name, initial, last_name);
 
     int bugs = 100;
     double bug_rate = 1.2;
 
     printf("You have %d bugs at the imaginary rate of %f.\n", bugs, bug_rate);
+    long universe_of_defects = 1L * 1024L * 1024L * 1024L;
+    printf("The entire universe has %ld bugs. \n", universe_of_defects);
+
+    double expected_bugs = bugs * bug_rate;
+    printf("You are expected to have %f bugs.\n", expected_bugs);
+
+    double part_of_universe = expected_bugs / universe_of_defects;
+    printf("That is only a %e portion of the universe.\n", part_of_universe);
 
     return 0;
 }
