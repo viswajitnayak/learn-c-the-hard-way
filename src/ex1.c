893325ccb09649c03091a06c5f5d1a38d1a61227
Viswajit Nayak <pragmaticde@Viswajits-Mac-mini.local>
2022-06-12 15:05:40 +0530

    Learn C the hard way

--- src/ex1.c
+++ src/ex1.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /* This is a comment */
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
-	int distance =100;
+	int distance = 100;
 
 	// This is also a comment
 	printf("You are %d miles away. \n", distance);
 
 	return 0;
-
 }
